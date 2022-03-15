d2fac33c6698eec7a6e5930dd4a20273730b4a7e
HaeS <HaeS@DESKTOP-HKUCICJ>
2022-03-15 17:10:48 +0900

    home.jsp,cafeComment 삭제

--- cherry_market1/src/main/java/com/acorn/cherryM1/cafe/controller/CafeController.java
+++ cherry_market1/src/main/java/com/acorn/cherryM1/cafe/controller/CafeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.acorn.cherryM1.cafe.dto.CafeCommentDto;
+
 import com.acorn.cherryM1.cafe.dto.CafeDto;
 import com.acorn.cherryM1.cafe.service.CafeService;
 
@@ -22,34 +22,34 @@ import com.acorn.cherryM1.cafe.service.CafeService;
 
 @Controller
 public class CafeController {
-	@Autowired
-	private CafeService service;
-
-	@RequestMapping("/cafe/list")
-	public String getList(HttpServletRequest request) {
-
-		service.getList(request);
-
-		return "cafe/list";
-	}
+		@Autowired
+		private CafeService service;
 	
-	@RequestMapping("/cafe/private/insertform")
-	public String insertForm() {
-
-		return "cafe/insertform";
-	}
+		@RequestMapping("/cafe/list")
+		public String getList(HttpServletRequest request) {
 	
-	//새글 저장 요청 처리 
-	@RequestMapping("/cafe/private/insert")
-	public String insert(CafeDto dto, HttpSession session) {
-			//글 작성자는 세션에서 얻어낸다. 
-			String id=(String)session.getAttribute("id");
-			//CafeDto 객체에 글 작성자도 담기
-			dto.setWriter(id);
-			service.saveContent(dto);
-
-			return "cafe/insert";
-	}
+			service.getList(request);
+	
+			return "cafe/list";
+		}
+		
+		@RequestMapping("/cafe/private/insertform")
+		public String insertForm() {
+	
+			return "cafe/insertform";
+		}
+		
+		//새글 저장 요청 처리 
+		@RequestMapping("/cafe/private/insert")
+		public String insert(CafeDto dto, HttpSession session) {
+				//글 작성자는 세션에서 얻어낸다. 
+				String id=(String)session.getAttribute("id");
+				//CafeDto 객체에 글 작성자도 담기
+				dto.setWriter(id);
+				service.saveContent(dto);
+	
+				return "cafe/insert";
+		}
 	
 		//글 자세히 보기 요청 처리
 		@RequestMapping("/cafe/detail")
@@ -58,45 +58,6 @@ public class CafeController {
 			return "cafe/detail";
 		}
 		
-		//새로운 댓글 저장 요청 처리
-		@RequestMapping("/cafe/private/comment_insert")
-		public String commentInsert(HttpServletRequest request, 
-				@RequestParam int ref_group) {
-
-			service.saveComment(request);
-
-			return "redirect:/cafe/detail.do?num="+ref_group;
-		}
-		
-		//댓글 더보기 요청 처리
-		@RequestMapping("/cafe/ajax_comment_list")
-		public String ajaxCommentList(HttpServletRequest request) {
-
-			service.moreCommentList(request);
-
-			return "cafe/ajax_comment_list";
-		}
-		
-		//댓글 삭제 요청 처리
-		@RequestMapping("/cafe/private/comment_delete")
-		@ResponseBody
-		public Map<String, Object> commentDelete(HttpServletRequest request) {
-			service.deleteComment(request);
-			Map<String, Object> map=new HashMap<String, Object>();
-			map.put("isSuccess", true);
-			// {"isSuccess":true} 형식의 JSON 문자열이 응답되도록 한다. 
-			return map;
-		}
-		//댓글 수정 요청처리 (JSON 을 응답하도록 한다)
-		@RequestMapping("/cafe/private/comment_update")
-		@ResponseBody
-		public Map<String, Object> commentUpdate(CafeCommentDto dto){
-			service.updateComment(dto);
-			Map<String, Object> map=new HashMap<String, Object>();
-			map.put("isSuccess", true);
-			// {"isSuccess":true} 형식의 JSON 문자열이 응답되도록 한다. 
-			return map;
-		}
 		//카페글 삭제 요청 처리 
 		@RequestMapping("/cafe/private/delete")
 		public String delete(@RequestParam int num, HttpServletRequest request) {
--- cherry_market1/src/main/java/com/acorn/cherryM1/cafe/service/CafeService.java
+++ cherry_market1/src/main/java/com/acorn/cherryM1/cafe/service/CafeService.java
@@ -2,7 +2,6 @@ package com.acorn.cherryM1.cafe.service;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.acorn.cherryM1.cafe.dto.CafeCommentDto;
 import com.acorn.cherryM1.cafe.dto.CafeDto;
 
 public interface CafeService {
@@ -11,10 +10,6 @@ public interface CafeService {
 	public void saveContent(CafeDto dto);
 	public void updateContent(CafeDto dto);
 	public void deleteContent(int num, HttpServletRequest request);
-	public void saveComment(HttpServletRequest request);//댓글 저장 
-	public void deleteComment(HttpServletRequest request);//댓글 삭제
-	public void updateComment(CafeCommentDto dto);//댓글 수정
-	public void moreCommentList(HttpServletRequest request);//댓글 추가 응답
 	public void getData(HttpServletRequest request); //글 수정하기 위해 정보 불러오는 기능
 }
 
